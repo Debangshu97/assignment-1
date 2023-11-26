e603aca7cb075829f12bf4ed5093872fe77997a9
Debangshu <kanthadebangshu97@gmail.com>
2023-11-26 16:51:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [xAxis, setXAxis] = useState(0)
+  const [yAxis, setYAxis] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div className='App'>
+      <button className='horizontal' onClick={() => setYAxis(yAxis - 50)} disabled={yAxis === 0}>Up</button>
+      <div className='container'>
+        <button className='vertical' onClick={() => setXAxis(xAxis - 50)} disabled={xAxis === 0}> Left</button>
+        <div className='inner-container'>
+          <div className='box' style={{ left: xAxis, top: yAxis }}></div>
+          </div>
+          <button className='vertical' onClick={() => setXAxis(xAxis + 50)} disabled={xAxis === 450}>Right</button>
+        </div>
+        <button className='horizontal' onClick={() => setYAxis(yAxis + 50)} disabled={yAxis === 450}>Bottom</button>
+      </div>
   );
 }
 
